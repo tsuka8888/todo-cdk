8c42dd335875fcec9eadcde6dd96383f6f430b07
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2022-07-14 17:13:02 +0900

    [modify]APIGatewayにカスタムドメインを設定

--- lib/todo-cdk-stack.ts
+++ lib/todo-cdk-stack.ts
@@ -1,5 +1,7 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib'
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager'
+import * as ssm from 'aws-cdk-lib/aws-ssm'
 import { TodoConstructor } from './constructor/todo/todoConstructor'
 
 export class TodoCdkStack extends Stack {
@@ -8,8 +10,26 @@ export class TodoCdkStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props)
 
+    const domainName = ssm.StringParameter.valueForStringParameter(
+      this,
+      'api_domain_name'
+    )
+    const domainCertificationArn = ssm.StringParameter.valueForStringParameter(
+      this,
+      'api_domain_certification_arn'
+    )
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      'certificate',
+      domainCertificationArn
+    )
+
     this.agw = new apigateway.RestApi(this, 'todoApi', {
       restApiName: 'Todo API',
+      domainName: {
+        domainName,
+        certificate,
+      },
     })
 
     new TodoConstructor(this)
